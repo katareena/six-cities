042bc41b7180b35067be1132968ee4f2fba2ab7e
katareena <w10160177@gmail.com>
2022-05-19 11:13:38 +0300

    switch to Redux Toolkit

--- project/src/index.js
+++ project/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from './services/api';
 import { redirect } from './store/middleware/redirect';
 import { Provider } from 'react-redux';
-import { composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from './store/root-reducer';
 import { requireAuthorization } from './store/action';
 import { fetchOffersList, checkAuth } from './store/api-actions';
-
 import { CITIES, AuthorizationStatus } from './constants/common';
 import App from './components/app/app';
 
@@ -17,13 +14,15 @@ const api = createAPI(
   () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH)),
 );
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(redirect),
-  ),
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }).concat(redirect),
+});
 
 store.dispatch(checkAuth());
 store.dispatch(fetchOffersList());
