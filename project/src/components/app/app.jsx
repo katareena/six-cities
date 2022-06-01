ca93c043239aa064a616e7ec5732bd63bc02c15b
katareena <w10160177@gmail.com>
2022-06-01 20:10:01 +0300

    move BrowserRouter to the index.js for convenient routing testing in app.js

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Router as BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from './../../constants/common';
 import PrivateRoute from './private-route/private-route';
-import browserHistory from '../../browser-history';
 import SignIn from './sign-in/sign-in';
 import Main from './main/main.jsx';
 import OfferPage from './offer-page/offer-page';
@@ -30,31 +29,29 @@ function App() {
   const isOffersLoaded = useSelector(getIsOffersLoaded);
 
   return (
-    <BrowserRouter history={browserHistory}>
-      <Switch>
+    <Switch>
 
-        <Route exact path={AppRoute.LOGIN}>
-          <PrivateRoute exact path={AppRoute.LOGIN}
-            allowedStatus={AuthorizationStatus.NO_AUTH}
-            redirect={AppRoute.ROOT}
-            render={() => <SignIn/>}
-          />
-        </Route>
-
-        <PrivateRoute exact path={AppRoute.FAVORITES}
-          allowedStatus={AuthorizationStatus.AUTH}
-          redirect={AppRoute.LOGIN}
-          render={() => <Favorites/>}
+      <Route exact path={AppRoute.LOGIN}>
+        <PrivateRoute exact path={AppRoute.LOGIN}
+          allowedStatus={AuthorizationStatus.NO_AUTH}
+          redirect={AppRoute.ROOT}
+          render={() => <SignIn/>}
         />
+      </Route>
 
-        <Route exact path={AppRoute.ROOT} render={() => renderMain(offers, isOffersLoaded)}/>
+      <PrivateRoute exact path={AppRoute.FAVORITES}
+        allowedStatus={AuthorizationStatus.AUTH}
+        redirect={AppRoute.LOGIN}
+        render={() => <Favorites/>}
+      />
 
-        <Route exact path={AppRoute.OFFER} render={() => <OfferPage />} />
+      <Route exact path={AppRoute.ROOT} render={() => renderMain(offers, isOffersLoaded)}/>
 
-        <Route render={() => <NotFoundScreen />} />
+      <Route exact path={AppRoute.OFFER} render={() => <OfferPage />} />
 
-      </Switch>
-    </BrowserRouter>
+      <Route render={() => <NotFoundScreen />} />
+
+    </Switch>
   );
 }
 
--- project/src/index.js
+++ project/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router as BrowserRouter } from 'react-router-dom';
+import browserHistory from '../../browser-history';
 import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from './services/api';
 import { redirect } from './store/middleware/redirect';
@@ -30,7 +32,9 @@ store.dispatch(fetchOffersList());
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter history={browserHistory}>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
